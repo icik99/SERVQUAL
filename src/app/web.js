c5f7fe27699f39d61bd116600a5cd1f2a56374a4
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2024-07-26 13:15:06 +0700

    feature : add web option

--- src/app/web.js
+++ src/app/web.js
@@ -5,7 +5,14 @@ import {errorMidleware} from "../middleware/error-middleware.js";
 
 export const web = express();
 web.use(express.json());
-web.use(publicRouter);
 web.use(cors())
+web.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 web.use("/", errorMidleware );
+
+
+web.use(publicRouter);
 
